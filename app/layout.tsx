c4df0552cffd0845461af844764d683275f856d6
gcdavid <gcdavid17@gmail.com>
2024-07-21 11:35:05 -0400

    authentication setup by clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 import { Metadata } from "next";
 import Home from "./(root)/page";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -18,13 +20,23 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn("min-h-screen font-sans antialiased", fontSans.variable)}
-      >
-        <Home />
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: { colorPrimary: "#3371FF", fontSize: "16px" },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <Home />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
